022da61f2dcd7a03ba483fa2f395c849f63c2ede
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-09-30 15:18:17 -0500

    Added users seeder

---
+++ users_seeder.sql
@@ -0,0 +1,11 @@
+USE adlister_db;
+
+INSERT INTO users
+(username, email, password)
+VALUES
+('admin', 'admin@megami.nd', 'superman'),
+('brian', 'brian@megami.nd', 'barton'),
+('jesse', 'jesse@megami.nd', 'garza'),
+('tim', 'tim@megami.nd', 'boudreaux'),
+('trevor', 'trevor@megami.nd', 'arnold');
+SELECT * FROM users;
