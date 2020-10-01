b4b18e492ad479f932fffaf0fba38bbc0ca73dc6
Trevor Arnold <arnold.j.trevor@gmail.com>
2020-10-01 01:01:43 -0500

    Detail Servlet Set Up and AdDetailsPage created

--- src/main/java/com/codeup/adlister/controllers/DetailsAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/DetailsAdServlet.java
@@ -1,6 +1,7 @@
 package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
@@ -10,17 +11,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "adDetails", urlPatterns = "/details")
+@WebServlet(name = "DetailsAdServlet", urlPatterns = "/details")
 public class DetailsAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("ads", DaoFactory.getAdsDao().all());
 
         String adId = request.getParameter("id");
-        Long id = Long.parseLong(adId);
-        request.setAttribute("ad", DaoFactory.getAdsDao().getUserInfo(id));
-        request.setAttribute("user", DaoFactory.getUsersDao().findUserwithAdId(id));
-        request.setAttribute("category", DaoFactory.getCategoriesDao().findCategorywithAdId(id));
-        request.getRequestDispatcher("/WEB-INF/ads/adDetail.jsp").forward(request, response);
+        int id = Integer.parseInt(adId);
+        request.setAttribute("user", DaoFactory.getAdsDao().getUserInfo(id));
+        //request.setAttribute("user", DaoFactory.getUsersDao().findUserwithAdId(id));
+        //request.setAttribute("category", DaoFactory.getCategoriesDao().findCategorywithAdId(id));
+        request.getRequestDispatcher("/WEB-INF/ads/adDetails.jsp").forward(request, response);
     }
 }
 
